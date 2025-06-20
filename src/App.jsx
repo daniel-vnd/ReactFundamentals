7bc57e29b6a7181bf3b34fc3d45a64e5da5d69c9
Daniel Velnic <velnicd@yahoo.com>
2025-06-20 17:07:03 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Contact from './component/Contact';
+import Footer from './component/Footer';
+import Header from './component/Header';
+import Hero from './component/Hero';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div>
+            <img src="https://cdn.fs.teachablecdn.com/uCoqmkHsS5OW2rTgbQIo" alt="" />
+        </div>
+    );
+};
+
+export default Hero;
