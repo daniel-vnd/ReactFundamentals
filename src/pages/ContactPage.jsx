95cf4582c82004987e27d87acf88baf02490574f
Daniel Velnic <velnicd@yahoo.com>
2025-06-30 20:19:05 +0300

    31. Applying React Router Dom

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>This is the Contact Page</h1>
+        </div>
+    );
+};
+
+export default ContactPage;<h1>This is the Contact Page</h1>
