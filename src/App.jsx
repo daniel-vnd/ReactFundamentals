6cfe139371e53b27e6227a805808da6a497e58cf
Daniel Velnic <velnicd@yahoo.com>
2025-06-30 20:05:30 +0300

    29. Managing Click Event - 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,16 @@ import Hero from './component/Hero';
 // 29. Managing Click Event
 
 const App = () => {
-	const BtnClick = () => {
-		alert("Hello Ariyan!");
+	function Demo() {
+		alert('hi ariyan')
 	}
 
 	return (
 		<div>
-			<button onClick={() => {
-				alert('hi ariyan')
-			}}>Submit</button>
+			<button onClick={Demo}>Submit</button>
 		</div>
 	);
+
 };
 
 export default App;
