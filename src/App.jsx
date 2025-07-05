3653ad376949f1266c29789ebefdd5ccc8a52bfe
Daniel Velnic <velnicd@yahoo.com>
2025-07-05 20:43:51 +0300

    45. [Hook] useState - managing like a pro part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,25 @@
 import { useState } from 'react';
 
-// 43. [Hook] useState - working with imutable array - part 2
+// 45. [Hook] useState - managing like pro part 1
 
 const App = () => {
-
-	const [list, setList] = useState(['a', 'b']);
+	let [formObject, setFormObject] = useState({fName:"",lName:"",city:"",gender: ""});
 	
-	const [item, setItem] = useState("");
-		
-	const AddToList = () => {
-		list.push(item);
-		
-		setList([...list]);
-	}
-	
-	const RemoveItem = (index) => {
-		alert(index);
-		list.splice(index, 1);
-
-		setList([...list]);
-	}
-		
 	return (
-		<div>
-			<table>
-				<tbody>
-					{
-						list.length !== 0 ? (
-							list.map((element, index) => {
-								return (
-									<tr>
-										<td>{element}</td>
-										<td><button onClick={()=>{
-											RemoveItem(index);
-										}}>Remove</button></td>
-									</tr>
-								)
-							})
-							
-							
-						) : (<tr></tr>)
-					}
-				</tbody>
-			</table>
-			<p>Item: {item}</p>
-			<p>List length: {list.length}</p>
-			<input placeholder='Item' onChange={(e) => setItem(e.target.value)} />
-			<button onClick={AddToList}>Change</button>
+		<div className='container'>
+		<form action="">
+			<input value={formObject.fName} placeholder='First Name' />
+			<input value={formObject.lName} placeholder='Last Name' />
+			<select value={formObject.city}>
+				<option value="">Choose City</option>
+				<option value="Rajbari">Rajbari</option>
+				<option value="Dhaka">Dhaka</option>
+			</select>
+			<input checked={formObject.gender === "Male"} type="radio" name="gender" /> Male
+			<input checked={formObject.gender === "Female"} type="radio" name="gender" /> Female
+			<br />
+			<button type='submit'>Submit</button>
+		</form>
 		</div>
 	)
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import "./assets/css/style.css"
 import "bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'milligram/dist/milligram.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
