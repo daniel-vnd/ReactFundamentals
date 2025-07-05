2d88dc8e33ff6e675bb518865d3722aa5b2704d7
Daniel Velnic <velnicd@yahoo.com>
2025-07-05 20:53:12 +0300

    47. [Hook] useEffect - calling api using async await

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from 'react';
 
-// 47. [Hook] useEffect - calling api using promises
+// 47. [Hook] useEffect - calling api using async await
 
 const App = () => {
 	let [data, setData] = useState({});
 
 	// executes only when the components loads and when the value that is between the brackets "[]" will change
 	useEffect(() => {
-		fetch('https://dummyjson.com/products/1')
-			.then(res => res.json())
-			.then(json => setData(json));
+		// here we will use the imediate invoked function
+		(async () => {
+			let response = await fetch('https://dummyjson.com/products/1');
+			let json = await response.json();
+			
+			setData(json);
+		})();
+
 	}, []);
 	
 	return (
