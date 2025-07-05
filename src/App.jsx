22d7e14076e5fa45059c652fa2aba7c0c011f822
Daniel Velnic <velnicd@yahoo.com>
2025-07-05 20:51:25 +0300

    47. [Hook] useEffect - calling api using promises

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-// 47. [Hook] useEffect - understanding arguments and uses
+// 47. [Hook] useEffect - calling api using promises
 
 const App = () => {
-	// executes twice and on each modificatio of the state
-	// when we use "React.StrictMode" the component will be loaded 2 times
-	useEffect(() => {
-		console.log("Hi Ariyan");
-	});
+	let [data, setData] = useState({});
 
 	// executes only when the components loads and when the value that is between the brackets "[]" will change
 	useEffect(() => {
-		console.log("Hi Ariyan");
-	}, [4]);
+		fetch('https://dummyjson.com/products/1')
+			.then(res => res.json())
+			.then(json => setData(json));
+	}, []);
 	
 	return (
 		<div>
-			
+			{JSON.stringify(data)}
 		</div>
 	)
-
 };
 
 export default App;
