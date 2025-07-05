7349580ce9a98b2cbeb1deaf7963d9102528c823
Daniel Velnic <velnicd@yahoo.com>
2025-07-05 20:11:10 +0300

    41. [Hook] useState - call function when click btn

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,14 @@ import { useState } from 'react';
 const App = () => {
 	const [number, setNumber] = useState(1);
 	
+	const changeNumber = () => {
+		setNumber(number + 1);
+	}
+	
 	return (
 		<div>
 			<h1>Number: {number}</h1>
-			<button onClick={() => setNumber(number + 1)}>Click</button>
+			<button onClick={changeNumber}>Click</button>
 		</div>
 	)
 };
