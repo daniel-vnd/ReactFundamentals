3537d97e5e5515063376016b4829d356d9d22429
Daniel Velnic <velnicd@yahoo.com>
2025-07-05 20:14:44 +0300

    41. [Hook] useState - working with imutable object

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
 import { useState } from 'react';
 
-// 41. [Hook] useState - understand inside 
+// 42. [Hook] useState - working with imutable object 
 
 const App = () => {
-	const [number, setNumber] = useState(1);
+
+	const [myObj, setMyObj] = useState({
+		key1: "Ariyan 1",
+		key2: "Ariyan 2",
+		key3: "Ariyan 3",
+	});
 	
-	const changeNumber = () => {
-		setNumber(number + 1);
+	const change = () => {
+		// Update all the object values
+		setMyObj({
+			key1: "Kazi Ariyan 1",
+			key2: "Kazi Ariyan 2",
+			key3: "Kazi Ariyan 3",
+		
+		});
+		
+		// Update a specific key of the object
+		setMyObj(previousObj => ({
+			 ...previousObj,
+			 key1: "hi how are you"
+		}));
+		
+		// Update multiple keys of the object
+		setMyObj(previousObj => ({
+			 ...previousObj,
+			 key1: "hi how are you",
+			 key2: "what is your name"
+		}));
 	}
 	
 	return (
 		<div>
-			<h1>Number: {number}</h1>
-			<button onClick={changeNumber}>Click</button>
+			<h1>Number: {myObj.key1}</h1>
+			<h1>{myObj.key2}</h1>
+			<h1>{myObj.key3}</h1>
+			<button onClick={change}>Change</button>
 		</div>
 	)
 };
