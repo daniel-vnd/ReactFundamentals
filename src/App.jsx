aad649e9fe80a4c6bdd82d750a209132b17f8325
Daniel Velnic <velnicd@yahoo.com>
2025-07-05 19:57:05 +0300

    38. [Hook] useRef working with input element - we target the reference

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,16 @@ const App = () => {
 	let lastName = useRef();
 	
 	const change = () => {
-		let fName = firstName.current.value
-		let lName = lastName.current.value
+		let fName = firstName.value
+		let lName = lastName.value
 		
 		alert(fName + " " + lName);
 	}
 
 	return (
 		<div>
-			<input ref={firstName} placeholder='First Name' /> <br />
-			<input ref={lastName} placeholder='Last Name' /> <br />
+			<input ref={(a) => firstName=a} placeholder='First Name' /> <br />
+			<input ref={(a) => lastName=a} placeholder='Last Name' /> <br />
 			
 			<button onClick={change}>Click</button>
 		</div>
